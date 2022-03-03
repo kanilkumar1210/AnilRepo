92a1ca4bea8e30542a3f3979dfdaaa76338dc5b2
kanilkumar1210 <100669529+kanilkumar1210@users.noreply.github.com>
2022-03-03 13:16:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
   public static void main(String args[]){
      System.out.println("Hello Jenkins");
-     System.out.println("I am from GitHub");
+     System.out.println("I am from GitHub123");
 }
 }
