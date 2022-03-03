b3cdbbe213df52278db9d817cb829af1af1e0463
kanilkumar1210 <100669529+kanilkumar1210@users.noreply.github.com>
2022-03-03 15:48:37 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+  public static void main(String args[]){
+     System.out.println("Hello Jenkins");
+     System.out.println("How are You");
+      System.out.println("Hello ngrok");
+    
+}
+}
